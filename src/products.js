b71a8df6378aee400debba571e2667e4e3b6fa69
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2024-08-19 15:26:22 +0300

    edited data

--- src/products.js
+++ src/products.js
@@ -1,13 +1,13 @@
 import cookieImg from "./images/cookie.jpeg"
  const products = [
-    {name: "Chocolate Chips Cookies 1", price: 1.99, image: cookieImg},
-    {name: "Chocolate Chips Cookies 2", price: 2.99, image: "https://cdn.craft.cloud/224393fa-1975-4d80-9067-ada3cb5948ca/assets/detail_bb_Chocolate_Chip_Cookies_2-0003_2022.jpg"},
-    {name: "Chocolate Chips Cookies 3", price: 3.99, image: "https://www.shutterstock.com/image-photo/three-chocolate-chip-cookies-isolated-260nw-649594483.jpg"},
-    {name: "Chocolate Chips Cookies 4", price: 4.99, image: "https://www.dessertfortwo.com/wp-content/uploads/2016/04/small-batch-chocolate-chip-cookies-1-720x720.jpg"},
-    {name: "Chocolate Chips Cookies 5", price: 5.99, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVb9fPvKnwSiqJneWJjct16FUurLYsCbhVyg&s"},
-    {name: "Chocolate Chips Cookies 6", price: 6.99, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkXqJDYCLvdRT4rExOgKPDGvglXxXWTLFP8A&s"},
-    {name: "Chocolate Chips Cookies 7", price: 7.99, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf19uxBBPCZMUDhQtg5a40jtSofeEHuRgUnA&s"},
-    {name: "Chocolate Chips Cookies 8", price: 8.99, image: "https://thevegan8.com/wp-content/uploads/2018/03/vegan-peanut-butter-chocolate-chip-cookies5-.jpg"},
+    {name: "Cookie x1", price: 1.99, image: cookieImg},
+    {name: "Cookie x2", price: 2.99, image: "https://cdn.craft.cloud/224393fa-1975-4d80-9067-ada3cb5948ca/assets/detail_bb_Chocolate_Chip_Cookies_2-0003_2022.jpg"},
+    {name: "Cookie x3", price: 3.99, image: "https://www.shutterstock.com/image-photo/three-chocolate-chip-cookies-isolated-260nw-649594483.jpg"},
+    {name: "Cookie x4", price: 4.99, image: "https://www.dessertfortwo.com/wp-content/uploads/2016/04/small-batch-chocolate-chip-cookies-1-720x720.jpg"},
+    {name: "Cookie x5", price: 5.99, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVb9fPvKnwSiqJneWJjct16FUurLYsCbhVyg&s"},
+    {name: "Cookie x6", price: 6.99, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkXqJDYCLvdRT4rExOgKPDGvglXxXWTLFP8A&s"},
+    {name: "Cookie x7", price: 7.99, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf19uxBBPCZMUDhQtg5a40jtSofeEHuRgUnA&s"},
+    {name: "Cookie x8", price: 8.99, image: "https://thevegan8.com/wp-content/uploads/2018/03/vegan-peanut-butter-chocolate-chip-cookies5-.jpg"},
 
 ]
 
